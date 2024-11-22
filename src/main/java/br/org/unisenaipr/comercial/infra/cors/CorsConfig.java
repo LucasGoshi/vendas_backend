b6fb59c5bbb92948fda46c916f08e94fddf33a59
Lucas Goshi <143546557+LucasGoshi@users.noreply.github.com>
2024-11-21 21:52:20 -0300

    Update CorsConfig.java

--- src/main/java/br/org/unisenaipr/comercial/infra/cors/CorsConfig.java
+++ src/main/java/br/org/unisenaipr/comercial/infra/cors/CorsConfig.java
@@ -1,4 +1,4 @@
-package br.org.unisenaipr.comercial.infra;
+package br.org.unisenaipr.comercial.infra.cors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Libera os endpoints iniciados por /api
-                .allowedOrigins("*") // Substitua pelo URL do seu frontend
+                .allowedOrigins("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
